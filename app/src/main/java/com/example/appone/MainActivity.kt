d9438fb1c3c66d74348784e6fb75d389d36627a1
paal <vangalkin@gmail.com>
2021-12-09 08:16:05 +0300

    SecondScreen & button

--- app/src/main/java/com/example/appone/MainActivity.kt
+++ app/src/main/java/com/example/appone/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.appone
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.next_button)
+        button.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
